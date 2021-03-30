844b8a1880fff84dbe7b87849ab0361d6ff3d60f
alexrese <alexrese@outlook.com>
2021-03-30 16:03:52 -0300

    aula 11

---
+++ aula11/ex08.js
@@ -0,0 +1,6 @@
+var vel = 60.5;
+console.log(`A velocidade do se carro é ${vel}km/h`);
+if(vel > 60){
+    console.log("Você ultrapassou a velicidade permitida. MULTADO");
+}
+console.log("Dirija sempre usando o cindo de segurança!");
